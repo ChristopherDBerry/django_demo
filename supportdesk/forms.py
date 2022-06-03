b2b5de6db9b721b00d3cbf9d68fe77f3f1268dc4
chris <cberry@sitemorse.com>
2022-06-03 09:58:35 +0200

    supportdesk/forms.py Cleanup

--- supportdesk/forms.py
+++ supportdesk/forms.py
@@ -1,11 +1,14 @@
+""" Forms for supportdesk app """
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Field, Fieldset, ButtonHolder, Submit
+from crispy_forms.layout import Layout, Field, Submit
 
 class CustomSwitch(Field):
+    """Custom toggle switch replaces checkbox """
     template = 'supportdesk/custom_switch.html'
 
 class ClientAddRequestForm(forms.Form):
+    """ Form for client to add support request"""
     summary = forms.CharField(label="Summary", max_length=100)
     description = forms.CharField(label="Description", widget=forms.Textarea)
     high_priority = forms.BooleanField(
